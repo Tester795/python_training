dade384dbd4d0d6110eebfda541d5e5f11d6135c
Zubakova Ekaterina <zubakova.ekaterina96@gmail.com>
2021-12-25 12:23:28 +0300

    Созданы пакеты для разных зон ответственности

--- fixture/application.py
+++ fixture/application.py
@@ -24,16 +24,9 @@ class Application:
         # ini group creation
         wd.find_element(By.NAME, "new").click()
         # fill group form
-        wd.find_element(By.NAME, "group_name").click()
-        wd.find_element(By.NAME, "group_name").clear()
         wd.find_element(By.NAME, "group_name").send_keys(group.name)
-        wd.find_element(By.NAME, "group_header").click()
         wd.find_element(By.XPATH, "//form[@action='/addressbook/group.php']").click()
-        wd.find_element(By.NAME, "group_header").click()
-        wd.find_element(By.NAME, "group_header").clear()
         wd.find_element(By.NAME, "group_header").send_keys(group.header)
-        wd.find_element(By.NAME, "group_footer").click()
-        wd.find_element(By.NAME, "group_footer").clear()
         wd.find_element(By.NAME, "group_footer").send_keys(group.footer)
         # submit group creation
         wd.find_element(By.NAME, "submit").click()
